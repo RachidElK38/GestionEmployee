35766ce85ef570c8a33d34c12aaf8671e6d8f1c0
Rachid ElKhamlichi <92023649+RachidElK38@users.noreply.github.com>
2022-02-14 18:22:10 -0800

    Update header.php

--- gestionEmployees/PageConstantes/header.php
+++ gestionEmployees/PageConstantes/header.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,7 +11,7 @@
     <title>Hello, world!</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -20,15 +19,15 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="AjouterEmployee.php">Ajouter Employée</a>
+        <a class="nav-link text-white" href="AjouterEmployee.php">Ajouter Employée</a>
         
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="index.php">List Employés </span></a>
+      <a class="nav-link text-white" href="index.php">List Employés </span></a>
 
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link text-white" href="#">Rechercher Employé</a>
       </li>
      
     </ul>
